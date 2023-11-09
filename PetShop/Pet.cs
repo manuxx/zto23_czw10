43dd1673684bd1678106b82d0eb64ad0333b83ca
Bartosz Leśniewski <blesniewski@outlook.com>
2023-11-09 11:06:44 +0100

    Task 1

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -1,13 +1,25 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Training.DomainClasses
 {
-    public class Pet 
+    public class Pet : IEquatable<Pet>
     {
         public Sex sex;
         public string name { get; set; }
         public int yearOfBirth { get; set; }
         public float price { get; set; }
         public Species species { get; set; }
+
+        public bool Equals([AllowNull] Pet other)
+        {
+            if (ReferenceEquals(this, other))
+                return true;
+
+            if (other.name == name)
+                return true;
+
+            return false;
+        }
     }
 }
