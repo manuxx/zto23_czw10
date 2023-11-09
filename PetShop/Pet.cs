1de9b00c41cb4e0bdbdf17502e228bc0e29da058
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-11-09 11:06:45 +0100

    pets: Fix failing UTs
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -2,12 +2,19 @@ using System;
 
 namespace Training.DomainClasses
 {
-    public class Pet 
+    public class Pet : IEquatable<Pet>
     {
         public Sex sex;
         public string name { get; set; }
         public int yearOfBirth { get; set; }
         public float price { get; set; }
         public Species species { get; set; }
+
+        bool IEquatable<Pet>.Equals(Pet other)
+        {
+            if (ReferenceEquals(this, other)) return true;
+            if (other.name == this.name) return true;
+            return false;
+        }
     }
 }
