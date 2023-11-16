8c719f4f98bd0aeee25aba0775b25c6b8ccc9b82
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-11-16 11:39:41 +0100

    Zadanie drugie
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -33,6 +33,24 @@ namespace Training.DomainClasses
             }
 
         }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                {
+                    yield return pet;
+                }
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            var ret = new List<Pet>(_petsInTheStore);
+            ret.Sort((p1, p2) => p1.name.CompareTo(p2.name));
+            return ret;
+        }
     }
 
     public class ReadOnly : IEnumerable<Pet>
