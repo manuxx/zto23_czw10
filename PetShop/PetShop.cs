37f7072c275c3a910dde90181636dc7deb3cfaaf
Pyerdowah <zosiasamosia209@gmail.com>
2023-11-16 12:17:39 +0100

    zad4

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -30,6 +30,7 @@ namespace Training.DomainClasses
                         return;
                     }
                 }
+
                 _petsInTheStore.Add(newPet);
             }
 
@@ -46,6 +47,62 @@ namespace Training.DomainClasses
         {
             foreach (var pet in _petsInTheStore)
                 if (pet.species == Species.Cat)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Mouse)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.sex == Sex.Female)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species == Species.Cat || pet.species == Species.Dog)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.species != Species.Mouse)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.yearOfBirth > 2010)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.yearOfBirth > 2010 && pet.species == Species.Dog)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.sex == Sex.Male && pet.species == Species.Dog)
+                    yield return pet;
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            foreach (var pet in _petsInTheStore)
+                if (pet.yearOfBirth > 2011 || pet.species == Species.Rabbit)
                     yield return pet;
         }
     }
