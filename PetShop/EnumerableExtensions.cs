35ae53078a3f8b31d49a798258cbdaecb7116d59
manuxx <manus@pg.gda.pl>
2023-11-16 11:06:59 +0100

    wyrefaktoryowana generyczna extension method

---
+++ PetShop/EnumerableExtensions.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Training.DomainClasses;
+
+public static class EnumerableExtensions
+{
+
+    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
+    {
+        foreach (var item in items)
+        {
+            yield return item;
+        }
+    }
+}
