14f173563646c18b39737f11eeef1078ada6967b
Piotr Sieński <s184297@student.pg.edu.pl>
2023-11-16 11:55:15 +0100

    Zad3

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -33,6 +33,14 @@ namespace Training.DomainClasses
             }
 
         }
+        public IEnumerable<Pet> AllThatSatisfy(Func<Pet, bool> checkFunc)
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (checkFunc(pet))
+                    yield return pet;
+            }
+        }
 
         public IEnumerable<Pet> AllCats()
         {
@@ -48,6 +56,46 @@ namespace Training.DomainClasses
             var ret = new List<Pet>(_petsInTheStore);
             ret.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
             return ret;
+        }
+
+        public IEnumerable<Pet> AllMice()
+        {
+            return AllThatSatisfy(pet => pet.species == Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllFemalePets()
+        {
+            return AllThatSatisfy(pet => pet.sex == Sex.Female);
+        }
+
+        public IEnumerable<Pet> AllCatsOrDogs()
+        {
+            return AllThatSatisfy(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+        }
+
+        public IEnumerable<Pet> AllPetsButNotMice()
+        {
+            return AllThatSatisfy(pet => pet.species != Species.Mouse);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2010()
+        {
+            return AllThatSatisfy(pet => pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllDogsBornAfter2010()
+        {
+            return AllThatSatisfy(pet => pet.species == Species.Dog && pet.yearOfBirth > 2010);
+        }
+
+        public IEnumerable<Pet> AllMaleDogs()
+        {
+            return AllThatSatisfy(pet => pet.species == Species.Dog && pet.sex == Sex.Male);
+        }
+
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        {
+            return AllThatSatisfy(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
         }
     }
 
