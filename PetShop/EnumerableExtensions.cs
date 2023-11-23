233192f1f1e22b5d361d6804a555c65889d31f2e
manuxx <manus@pg.gda.pl>
2023-11-23 10:47:13 +0100

    wyrefaktoryzowana Filter do generycznej extension method

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Training.DomainClasses;
 
@@ -9,6 +10,15 @@ public static class EnumerableExtensions
         foreach (var item in items)
         {
             yield return item;
+        }
+    }
+
+    public static IEnumerable<TItem> Filter<TItem>(this IEnumerable<TItem> items, Func<TItem, bool> condition)
+    {
+        foreach (var item in items)
+        {
+            if (condition(item))
+                yield return item;
         }
     }
 }
