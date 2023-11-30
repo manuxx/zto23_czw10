b8165abca98cfbd418cef33763ca1d2ba11a5914
wojbara1 <wojbara1@pg.edu.pl>
2023-11-30 10:44:55 +0100

    koniunkcja i alternatywa

--- PetShop/Negation.cs
+++ PetShop/Negation.cs
@@ -2,7 +2,7 @@ namespace Training.DomainClasses;
 
 public class Negation<T> : Criteria<T>
 {
-    private Criteria<T> _criteria;
+    private readonly Criteria<T> _criteria;
         
     public Negation(Criteria<T> criteria)
     {
--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -60,23 +60,22 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCatsOrDogs()
         {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Cat || pet.species == Species.Dog));
+            return _petsInTheStore.ThatSatisfy(new Alternative<Pet>(Pet.IsASpeciesOf(Species.Cat), Pet.IsASpeciesOf(Species.Dog)));
         }
 
         public IEnumerable<Pet> AllPetsButNotMice()
         {
-            return _petsInTheStore.ThatSatisfy(Pet.IsNotASpeciesOf(Species.Mouse));
+            return _petsInTheStore.ThatSatisfy(new Negation<Pet>(new Pet.SpeciesCriteria(Species.Mouse)));
         }
-
-
+        
         public IEnumerable<Pet> AllDogsBornAfter2010()
         {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog && pet.yearOfBirth > 2010));
+            return _petsInTheStore.ThatSatisfy(new Conjunction<Pet>(Pet.IsASpeciesOf(Species.Dog), Pet.IsBornAfter(2010)));
         }
 
         public IEnumerable<Pet> AllMaleDogs()
         {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog && pet.sex == Sex.Male));
+            return _petsInTheStore.ThatSatisfy(new Conjunction<Pet>(Pet.IsASpeciesOf(Species.Dog), new Negation<Pet>(Pet.IsFemale())));
         }
 
         public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
