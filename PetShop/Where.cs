f4d0db8ee83da83e8087d42c89a22c797cf1ff2e
Jakub Kuczys <me@jacken.men>
2023-11-30 11:45:55 +0100

    Create non-generic Where

---
+++ PetShop/Where.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training.DomainClasses
+{
+    public class Where_Pet
+    {
+        public static CriteriaBuilder HasAn(Func<Pet, Species> propertySelector)
+        {
+            return new CriteriaBuilder(propertySelector);
+        }
+    }
+
+    public class CriteriaBuilder
+    {
+        private readonly Func<Pet, Species> _propertySelector;
+
+        public CriteriaBuilder(Func<Pet, Species> propertySelector)
+        {
+            _propertySelector = propertySelector;
+        }
+
+        public Criteria<Pet> EqualTo(Species species)
+        {
+            return new PredicateCriteria<Pet>(p => _propertySelector(p).Equals(species));
+        }
+    }
+}
--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -207,7 +207,7 @@ namespace Training.Specificaton
     {
         private It should_be_able_to_find_all_cats = () =>
         {
-            Criteria<Pet> criteria = Where<Pet>.HasAn(p => p.species).EqualTo(Species.Cat);
+            Criteria<Pet> criteria = Where_Pet.HasAn(p => p.species).EqualTo(Species.Cat);
             var foundPets = subject.AllPets().ThatSatisfy(criteria);
             foundPets.ShouldContainOnly(cat_Tom, cat_Jinx);
         };
