cee71d962b12a882773637dcaafe545851527244
Pyerdowah <zosiasamosia209@gmail.com>
2023-11-30 10:36:56 +0100

    zad1

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -2,108 +2,116 @@ using System;
 
 namespace Training.DomainClasses
 {
-    public class Pet : IEquatable<Pet>
-    {
-        public bool Equals(Pet other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return name == other.name;
-        }
+	public class Pet : IEquatable<Pet>
+	{
+		public bool Equals(Pet other)
+		{
+			if (ReferenceEquals(null, other)) return false;
+			if (ReferenceEquals(this, other)) return true;
+			return name == other.name;
+		}
 
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(null, obj)) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((Pet)obj);
-        }
+		public override bool Equals(object obj)
+		{
+			if (ReferenceEquals(null, obj)) return false;
+			if (ReferenceEquals(this, obj)) return true;
+			if (obj.GetType() != this.GetType()) return false;
+			return Equals((Pet)obj);
+		}
 
-        public override int GetHashCode()
-        {
-            return (name != null ? name.GetHashCode() : 0);
-        }
+		public override int GetHashCode()
+		{
+			return (name != null ? name.GetHashCode() : 0);
+		}
 
-        public static bool operator ==(Pet left, Pet right)
-        {
-            return Equals(left, right);
-        }
+		public static bool operator ==(Pet left, Pet right)
+		{
+			return Equals(left, right);
+		}
 
-        public static bool operator !=(Pet left, Pet right)
-        {
-            return !Equals(left, right);
-        }
+		public static bool operator !=(Pet left, Pet right)
+		{
+			return !Equals(left, right);
+		}
 
-        public Sex sex;
-        public string name { get; set; }
-        public int yearOfBirth { get; set; }
-        public float price { get; set; }
-        public Species species { get; set; }
+		public Sex sex;
+		public string name { get; set; }
+		public int yearOfBirth { get; set; }
+		public float price { get; set; }
+		public Species species { get; set; }
 
-        public static Criteria<Pet> IsASpeciesOf(Species species)
-        {
-            return new SpeciesCriteria(species);
-        }
+		public static Criteria<Pet> IsASpeciesOf(Species species)
+		{
+			return new SpeciesCriteria(species);
+		}
 
-        public static Criteria<Pet> IsBornAfter(int year)
-        {
-            return new BornCriteria(year);
-        }
+		public static Criteria<Pet> IsBornAfter(int year)
+		{
+			return new BornCriteria(year);
+		}
 
-        public static Criteria<Pet> IsFemale()
-        {
-            return new SexCriteria(Sex.Female);
-        }
+		public static Criteria<Pet> IsFemale()
+		{
+			return new SexCriteria(Sex.Female);
+		}
 
-        public static Predicate<Pet> IsNotASpeciesOf(Species species)
-        {
-            return pet => pet.species != species;
-        }
-        public class SexCriteria : Criteria<Pet>
-        {
-            private readonly Sex _sex;
+		public class SexCriteria : Criteria<Pet>
+		{
+			private readonly Sex _sex;
 
-            public SexCriteria(Sex sex)
-            {
-                _sex = sex;
-            }
+			public SexCriteria(Sex sex)
+			{
+				_sex = sex;
+			}
 
-            public bool IsSatisfiedBy(Pet pet)
-            {
-                return pet.sex == _sex;
-            }
-        }
+			public bool IsSatisfiedBy(Pet pet)
+			{
+				return pet.sex == _sex;
+			}
+		}
 
-        public class SpeciesCriteria : Criteria<Pet>
-        {
-            private readonly Species _species;
+		public class SpeciesCriteria : Criteria<Pet>
+		{
+			private readonly Species _species;
 
-            public SpeciesCriteria(Species species)
-            {
-                _species = species;
-            }
+			public SpeciesCriteria(Species species)
+			{
+				_species = species;
+			}
 
-            public bool IsSatisfiedBy(Pet pet)
-            {
-                return pet.species == _species;
-            }
-        }
+			public bool IsSatisfiedBy(Pet pet)
+			{
+				return pet.species == _species;
+			}
+		}
 
-        public class BornCriteria : Criteria<Pet>
-        {
-            private readonly int _year;
+		public class BornCriteria : Criteria<Pet>
+		{
+			private readonly int _year;
 
-            public BornCriteria(int year)
-            {
-                _year = year;
-            }
+			public BornCriteria(int year)
+			{
+				_year = year;
+			}
 
-            public bool IsSatisfiedBy(Pet pet)
-            {
-                return pet.yearOfBirth > _year;
-            }
-        }
-    }
+			public bool IsSatisfiedBy(Pet pet)
+			{
+				return pet.yearOfBirth > _year;
+			}
+		}
+	}
 
-    
+	public class Negation<TItem> : Criteria<TItem>
+	{
+		private readonly Criteria<TItem> _innerCriteria;
+		public Negation(Criteria<TItem> innerCriteria)
+		{
+			_innerCriteria = innerCriteria;
+		}
+
+		public bool IsSatisfiedBy(TItem item)
+		{
+			return !_innerCriteria.IsSatisfiedBy(item);
+		}
+	}
 }
