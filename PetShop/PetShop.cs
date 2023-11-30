523cb160082e04ce4488fc643af42391ee527f1c
Pyerdowah <zosiasamosia209@gmail.com>
2023-11-30 10:56:22 +0100

    zad2

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -3,85 +3,117 @@ using System.Collections.Generic;
 
 namespace Training.DomainClasses
 {
-    public class PetShop
-    {
-        private IList<Pet> _petsInTheStore;
+	public class PetShop
+	{
+		private IList<Pet> _petsInTheStore;
 
-        public PetShop(IList<Pet> petsInTheStore)
-        {
-            this._petsInTheStore = petsInTheStore;
-        }
+		public PetShop(IList<Pet> petsInTheStore)
+		{
+			this._petsInTheStore = petsInTheStore;
+		}
 
-        public IEnumerable<Pet> AllPets()
-        {
-            return new ReadOnly<Pet>(_petsInTheStore);
-        }
+		public IEnumerable<Pet> AllPets()
+		{
+			return new ReadOnly<Pet>(_petsInTheStore);
+		}
 
-        public void Add(Pet newPet)
-        {
-           
-                foreach (var pet in _petsInTheStore)
-                {
-                    if (pet.name == newPet.name)
-                    {
-                        return;
-                    }
-                }
-                _petsInTheStore.Add(newPet);
+		public void Add(Pet newPet)
+		{
+			foreach (var pet in _petsInTheStore)
+			{
+				if (pet.name == newPet.name)
+				{
+					return;
+				}
+			}
 
-        }
-
-
-        public IEnumerable<Pet> AllPetsSortedByName()
-        {
-            var ret = new List<Pet>(_petsInTheStore);
-            ret.Sort((p1,p2)=>p1.name.CompareTo(p2.name));
-            return ret;
-        }
-
-        public IEnumerable<Pet> AllCats()
-        {
-            return _petsInTheStore.ThatSatisfy(Pet.IsASpeciesOf(Species.Cat) );
-        }
-        public IEnumerable<Pet> AllMice()
-        {
-            return _petsInTheStore.ThatSatisfy(Pet.IsASpeciesOf(Species.Mouse));
-        }
-
-        public IEnumerable<Pet> AllPetsBornAfter2010()
-        {
-            return _petsInTheStore.ThatSatisfy(Pet.IsBornAfter(2010));
-        }
-
-        public IEnumerable<Pet> AllFemalePets()
-        {
-            return _petsInTheStore.ThatSatisfy(Pet.IsFemale());
-        }
-
-        public IEnumerable<Pet> AllCatsOrDogs()
-        {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Cat || pet.species == Species.Dog));
-        }
-
-        public IEnumerable<Pet> AllPetsButNotMice()
-        {
-            return _petsInTheStore.ThatSatisfy(new Negation<Pet>(Pet.IsASpeciesOf(Species.Mouse)));
-        }
+			_petsInTheStore.Add(newPet);
+		}
 
 
-        public IEnumerable<Pet> AllDogsBornAfter2010()
-        {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog && pet.yearOfBirth > 2010));
-        }
+		public IEnumerable<Pet> AllPetsSortedByName()
+		{
+			var ret = new List<Pet>(_petsInTheStore);
+			ret.Sort((p1, p2) => p1.name.CompareTo(p2.name));
+			return ret;
+		}
 
-        public IEnumerable<Pet> AllMaleDogs()
-        {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog && pet.sex == Sex.Male));
-        }
+		public IEnumerable<Pet> AllCats()
+		{
+			return _petsInTheStore.ThatSatisfy(Pet.IsASpeciesOf(Species.Cat));
+		}
 
-        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
-        {
-            return _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Rabbit || pet.yearOfBirth > 2011));
-        }
-    }
+		public IEnumerable<Pet> AllMice()
+		{
+			return _petsInTheStore.ThatSatisfy(Pet.IsASpeciesOf(Species.Mouse));
+		}
+
+		public IEnumerable<Pet> AllPetsBornAfter2010()
+		{
+			return _petsInTheStore.ThatSatisfy(Pet.IsBornAfter(2010));
+		}
+
+		public IEnumerable<Pet> AllFemalePets()
+		{
+			return _petsInTheStore.ThatSatisfy(Pet.IsFemale());
+		}
+
+		public IEnumerable<Pet> AllCatsOrDogs()
+		{
+			return _petsInTheStore.ThatSatisfy(new Alternative<Pet>(Pet.IsASpeciesOf(Species.Cat), Pet.IsASpeciesOf(Species.Dog)));
+		}
+
+		public IEnumerable<Pet> AllPetsButNotMice()
+		{
+			return _petsInTheStore.ThatSatisfy(new Negation<Pet>(Pet.IsASpeciesOf(Species.Mouse)));
+		}
+
+
+		public IEnumerable<Pet> AllDogsBornAfter2010()
+		{
+			return _petsInTheStore.ThatSatisfy(new Conjuction<Pet>(Pet.IsASpeciesOf(Species.Dog), Pet.IsBornAfter(2010)));
+		}
+
+		public IEnumerable<Pet> AllMaleDogs()
+		{
+			return _petsInTheStore.ThatSatisfy(new Conjuction<Pet>(Pet.IsASpeciesOf(Species.Dog), new Negation<Pet>(Pet.IsFemale())));
+		}
+
+		public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+		{
+			return _petsInTheStore.ThatSatisfy(new Alternative<Pet>(Pet.IsASpeciesOf(Species.Rabbit), Pet.IsBornAfter(2011)));
+		}
+	}
+
+	public class Alternative<TItem> : Criteria<TItem>
+	{
+		private readonly Criteria<TItem> _criteria1;
+		private readonly Criteria<TItem> _criteria2;
+		public Alternative(Criteria<TItem> criteria1, Criteria<TItem> criteria2)
+		{
+			_criteria1 = criteria1;
+			_criteria2 = criteria2;
+		}
+
+		public bool IsSatisfiedBy(TItem item)
+		{
+			return _criteria1.IsSatisfiedBy(item) || _criteria2.IsSatisfiedBy(item);
+		}
+	}
+
+	public class Conjuction<TItem> : Criteria<TItem>
+	{
+		private readonly Criteria<TItem> _criteria1;
+		private readonly Criteria<TItem> _criteria2;
+		public Conjuction(Criteria<TItem> criteria1, Criteria<TItem> criteria2)
+		{
+			_criteria1 = criteria1;
+			_criteria2 = criteria2;
+		}
+
+		public bool IsSatisfiedBy(TItem item)
+		{
+			return _criteria1.IsSatisfiedBy(item) && _criteria2.IsSatisfiedBy(item);
+		}
+	}
 }
