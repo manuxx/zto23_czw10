4ea7587207de6ff46bc52464686dc8ab06198a2e
manuxx <manus@pg.gda.pl>
2023-11-30 11:29:07 +0100

    koniec duzego refaktoringu - nie uzywamy predykatow

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -13,10 +13,7 @@ public static class EnumerableExtensions
         }
     }
 
-    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
-    {
-        return items.ThatSatisfy(new PredicateCriteria<TItem>(condition));
-    }
+  
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
     {
         foreach (var item in items)
