e2dbdaee71d4fae09b5ce6e4c4029dc56619aa8e
student <student@nmkzllab>
2023-11-30 10:51:32 +0100

    1 commit

--- PetShop/Alternative.cs
+++ PetShop/Alternative.cs
@@ -2,17 +2,17 @@ namespace Training.DomainClasses;
 
 public class Alternative<TItem> : Criteria<TItem>
 {
-    private readonly Criteria<TItem> _criteria1;
-    private readonly Criteria<TItem> _criteria2;
+    private readonly Criteria<TItem> _criteria_1;
+    private readonly Criteria<TItem> _criteria_2;
 
     public Alternative(Criteria<TItem> criteria1, Criteria<TItem> criteria2)
     {
-        _criteria1 = criteria1;
-        _criteria2 = criteria2;
+        _criteria_1 = criteria1;
+        _criteria_2 = criteria2;
     }
 
     public bool IsSatisfiedBy(TItem item)
     {
-        return _criteria1.IsSatisfiedBy(item) || _criteria2.IsSatisfiedBy(item);
+        return _criteria_1.IsSatisfiedBy(item) || _criteria_2.IsSatisfiedBy(item);
     }
 }
