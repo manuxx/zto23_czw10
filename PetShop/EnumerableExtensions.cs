e1ccb481f225679573bc4aeefe2ecac1dee6653f
Michał Iwanicki <iwanicki92@gmail.com>
2023-11-30 11:26:12 +0100

    remove unused methods

--- PetShop/EnumerableExtensions.cs
+++ PetShop/EnumerableExtensions.cs
@@ -4,19 +4,6 @@ using Training.DomainClasses;
 
 public static class EnumerableExtensions
 {
-
-    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
-    {
-        foreach (var item in items)
-        {
-            yield return item;
-        }
-    }
-
-    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> condition)
-    {
-        return items.ThatSatisfy(new PredicateCriteria<TItem>(condition));
-    }
     public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
     {
         foreach (var item in items)
